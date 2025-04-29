6c4ca1bc75df289fc3a818cdb6c15a39c3f049d6
mineproness <mineproclient@gmail.com>
2025-04-29 22:45:48 +0600

    Update connect.mjs

--- connect.mjs
+++ connect.mjs
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const client = new MongoClient(process.env.MONGO)
+const client = new MongoClient("mongodb+srv://proplayerhacker20:Mineproness2023@cluster0.x4dcstd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
 export default await client.connect()
